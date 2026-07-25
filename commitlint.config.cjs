54a3b15007270ce5ee4f1c26d4ad4e713ca42c6b
Yusuf Ali <yusufdupsc1@gmail.com>
2026-07-25 23:31:36 +0600

    fix: relax commitlint body max-line-length so workflow commits can succeed

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,8 @@
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [0, 'always', 1000],
+    'header-max-length': [2, 'always', 100],
+  },
 };
